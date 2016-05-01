3224e8bb47903ba1f8c53ece182abaab479c7736
Sacheendra Talluri <sacheendra.t@gmail.com>
2016-05-02 00:48:43 +0530

    changes env vars from tutum to dockercloud

--- scripts/configure_and_run.sh
+++ scripts/configure_and_run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-sed -i -e "s/localhost:80/$UPSTREAM-${TUTUM_CONTAINER_HOSTNAME##*-}:$UPSTREAM_PORT,over_http/" /pushpin/routes
+if [ -z "$DOCKERCLOUD_CONTAINER_HOSTNAME" ]; then
+	sed -i -e "s/localhost:80/$UPSTREAM:$UPSTREAM_PORT,over_http/" /pushpin/routes
+else
+	sed -i -e "s/localhost:80/$UPSTREAM-${DOCKERCLOUD_CONTAINER_HOSTNAME##*-}:$UPSTREAM_PORT,over_http/" /pushpin/routes
+fi
 
 exec /pushpin/pushpin --verbose
